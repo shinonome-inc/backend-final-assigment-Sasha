998772a376bcccf3602bff7d5818d10cedaeea49
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2024-05-19 08:01:32 +0900

    EmailFieldの括弧内がデフォルトで, blank = Falseになることを示すコメントを削除しました。

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    email = models.EmailField()  # デフォルトでblank=Falseになっている
+    email = models.EmailField()
 
 
 # class FriendShip(models.Model):
