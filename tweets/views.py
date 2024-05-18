237e325eab602b1ccf6be986d024508ccced824b
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2024-05-19 08:12:27 +0900

    ホーム画面の実装

--- tweets/views.py
+++ tweets/views.py
@@ -1,1 +1,8 @@
-# from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+
+class HomeView(LoginRequiredMixin, TemplateView):  # LoginRequiredMixinでログインしたユーザーのみhomeにアクセス可能
+    login_url = "accounts:login"
+    redirect_field_name = "next"  # ログイン後ホームページにリダイレクトされるようにする
+    template_name = "tweets/home.html"
