f8caa5d2ff9fa64af56404311b31a27dda7d94a0
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2024-05-20 14:43:31 +0900

    おそらく不必要なlogin_urlなどのクラスメンバを削除

--- tweets/views.py
+++ tweets/views.py
@@ -3,6 +3,4 @@ from django.views.generic import TemplateView
 
 
 class HomeView(LoginRequiredMixin, TemplateView):  # LoginRequiredMixinでログインしたユーザーのみhomeにアクセス可能
-    login_url = "accounts:login"
-    redirect_field_name = "next"  # ログイン後ホームページにリダイレクトされるようにする
     template_name = "tweets/home.html"
