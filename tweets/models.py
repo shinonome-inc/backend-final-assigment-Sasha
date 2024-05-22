e69a13998dfece8cb1fdfa71b8e028b78085438b
quinn-sasha <quinnsashakenta@gmail.com>
2024-05-22 18:20:28 +0900

    Tweetモデルのクラスメンバにauthorを加えました。

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 
 class Tweet(models.Model):
     content = models.TextField(max_length=280)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
