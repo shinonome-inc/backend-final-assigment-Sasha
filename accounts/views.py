c43363662623a6f26ffe061aee47953db5ead671
quinn-sasha <quinnsashakenta@gmail.com>
2024-05-23 21:15:43 +0900

    フォーマットを修正しました(何回目)

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,8 @@
+from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
-
-from django.conf import settings
 
 from .forms import SignupForm
 
