75f10c1d885e29b09d8bab7a35964fc28c7e2185
quinn-sasha <quinnsashakenta@gmail.com>
2024-05-24 19:49:42 +0900

    Tweetをインポートし忘れてた。

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
+from tweets.models import Tweet
 from .forms import SignupForm
 
 
