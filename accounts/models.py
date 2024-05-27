df1ce49a8762c6d078dcedc3e9a9e11b3e0378bd
quinn-sasha <quinnsashakenta@gmail.com>
2024-05-27 12:27:18 +0900

    Followモデルを実装

--- accounts/models.py
+++ accounts/models.py
@@ -6,4 +6,12 @@ class User(AbstractUser):
     email = models.EmailField()
 
 
-# class FriendShip(models.Model):
+class Follow(models.Model):
+    # followしているユーザー
+    follower = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    # followされているユーザー
+    followed = models.ForeignKey(User, related_name="being_followed", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("follower", "followed")  # 絶対にfollowerとfollowedは等しくなる
