f1e6a48cd2c6fe4a0e6f90925b2f7f66df46dd22
quinn-sasha <quinnsashakenta@gmail.com>
2024-06-02 11:56:52 +0900

    Likeモデルを作成

--- tweets/models.py
+++ tweets/models.py
@@ -7,3 +7,9 @@ class Tweet(models.Model):
     content = models.TextField(max_length=280)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    # likeとuser, tweetのモデル間関係は'one-to-many'
+    user = models.ForeignKey(User, related_name="liking_user", on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, related_name="liked_tweet", on_delete=models.CASCADE)
